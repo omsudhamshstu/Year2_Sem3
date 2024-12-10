d9435b948265299b77bb40c8114b8e9acbf340c8
Om Sudhamsh Padma <129719097+omsudhamsh@users.noreply.github.com>
2024-12-10 16:02:37 +0530

    Update roundrobbin.c

--- OS/CPU Scheduling Algorithms/roundrobbin.c
+++ OS/CPU Scheduling Algorithms/roundrobbin.c
@@ -50,3 +50,31 @@ int main() {
     findAverageTime(n, bt, quantum);
     return 0;
 }
+
+
+//Another Short code for Round Robbin CPU Scheduling.
+#include <stdio.h>
+
+void roundRobin(int n, int bt[], int tq) {
+    int rem[n], time = 0, done = 0;
+    for (int i = 0; i < n; i++) rem[i] = bt[i];
+
+    while (done < n) {
+        for (int i = 0; i < n; i++) {
+            if (rem[i] > 0) {
+                int exec = (rem[i] > tq) ? tq : rem[i];
+                time += exec;
+                rem[i] -= exec;
+                if (rem[i] == 0) done++;
+                printf("P%d executed for %d units, time: %d\n", i + 1, exec, time);
+            }
+        }
+    }
+}
+
+int main() {
+    int n = 3, tq = 2;
+    int bt[] = {5, 3, 8}; // Burst times
+    roundRobin(n, bt, tq);
+    return 0;
+}
