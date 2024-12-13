934a24964136780f4004987c69396d17ef601411
omsudhamshstu <ommsstudentnetwork@gmail.com>
2024-12-13 19:49:51 +0530

    All codes are organised

--- Py/Python/19remove_word_in_all_places.py
+++
@@ -1,9 +0,0 @@
-# ii) Remove the given word in all the places in a string?
-def remove_word(string, word):
-    return string.replace(word, "")
-
-input_string = input("Enter a string: ")
-word_to_remove = input("Enter the word to remove: ")
-
-result = remove_word(input_string, word_to_remove)
-print("Updated String:", result)
