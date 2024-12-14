d6ce4f64aaa71ca58babbe9383ade1b3bcc85bb6
Om Sudhamsh Padma <129719097+omsudhamsh@users.noreply.github.com>
2024-12-14 17:37:27 +0530

    Update 10GCD.py

--- Py/Python/Week-3/10GCD.py
+++ Py/Python/Week-3/10GCD.py
@@ -1,9 +1,8 @@
 # 9. Write a function called gcd that takes parameters a and b and returns their greatest common divisor.
-import math
 
 def gcd(a, b):
     while b:
         a, b = b, a % b
     return a
 
-print(gcd(24, 36))  # Output: 12
+print(gcd(24, 36))
