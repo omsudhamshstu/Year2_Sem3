057d16a6ea491492da473f30f17c4d8f4a59bc20
omsudhamsh <omsudhamsh@gmail.com>
2025-01-31 14:54:11 +0530

    codesJava

---
+++ JavaLab/OOPconcepts.java
@@ -0,0 +1,24 @@
+import java.util.*;
+    class Numbers {
+        int x, y;
+        Scanner in = new Scanner(System.in);
+        void read() {
+            System.out.println("Enter two integers: ");
+            x=in.nextInt();
+            y=in.nextInt();
+        }
+    }
+    
+    class Derived extends Numbers {
+        public void display() {
+            System.out.println("The sum of the two integers is: " + (x+y));
+            System.out.println("The Product of the two integers is: " + (x*y));
+        }
+    }
+    public class OOPconcepts {
+    public static void main(String[] args) {
+        Derived d1 = new Derived();
+        d1.read();
+        d1.display();
+    }
+}
