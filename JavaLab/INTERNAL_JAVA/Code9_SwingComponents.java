66ce3d34ac95434025278147c3db82fe279d1a3a
omsudhamsh <omsudhamsh@gmail.com>
2025-05-09 07:08:54 +0530

    Code9

--- JavaLab/INTERNAL_JAVA/Code9_SwingComponents.java
+++ JavaLab/INTERNAL_JAVA/Code9_SwingComponents.java
@@ -1,9 +1,18 @@
 import javax.swing.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
-public class Code9_SwingComponents {
+public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Swing Example");
         JButton button = new JButton("Click Me");
+        
+        button.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                button.setText("clicked");
+            }
+        });
+
         frame.add(button);
         frame.setSize(300, 200);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
