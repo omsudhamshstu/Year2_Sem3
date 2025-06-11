ae7b4a605e80f40acacf44e7110205fe2a85fc82
om_sudhamsh <ommsstudentnetwork@gmail.com>
2025-06-11 12:08:07 +0530

    Update Code11_Applet.java

--- JavaLab/INTERNAL_JAVA/Code11_Applet.java
+++ JavaLab/INTERNAL_JAVA/Code11_Applet.java
@@ -8,3 +8,13 @@ public class Code11_Applet extends Applet {
 }
 // This code wont be executed in the JDK 21.0 and later versions.
 // To run this code, you need to use the appletviewer tool or an older version of Java.
+
+<html>
+  <head>
+    <title>Hello Applet</title>
+  </head>
+  <body>
+    <applet code="Code11_Applet.class" width="300" height="100">
+    </applet>
+  </body>
+</html>
